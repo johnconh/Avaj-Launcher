74ddd8cb6272962b31aa4ba9423cdfeb28cffee9
johnconh <sevenballs@gmail.com>
2024-09-25 16:15:37 +0200

    extend throws

--- src/com/jdasilva/avaj/simulator/AircraftFactory.java
+++ src/com/jdasilva/avaj/simulator/AircraftFactory.java
@@ -2,6 +2,8 @@ package com.jdasilva.avaj.simulator;
 
 import java.io.PrintWriter;
 
+import com.jdasilva.avaj.exceptions.InvalidInputFormatException;
+
 public class AircraftFactory 
 {
     private static AircraftFactory instance = null;
@@ -16,7 +18,7 @@ public class AircraftFactory
         return instance;
     }
 
-    public Flyable newAircraft(String p_type, String p_name, Coordinates p_coordinates, PrintWriter p_writer) 
+    public Flyable newAircraft(String p_type, String p_name, Coordinates p_coordinates, PrintWriter p_writer) throws InvalidInputFormatException 
     {
         long id = idCounter++;
         String type = p_type.toLowerCase();
@@ -29,7 +31,7 @@ public class AircraftFactory
             case "baloon":
                 return new Baloon(id, p_name, p_coordinates, p_writer);
             default:
-                throw new IllegalArgumentException("Invalid type of aircraft");
+                throw new InvalidInputFormatException("Error: Invalid aircraft type.");
         }
     }
 }
